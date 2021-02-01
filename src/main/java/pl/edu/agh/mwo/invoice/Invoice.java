00c052ae2ff1f4d5f73d8a4e81c58f03755195fb
Konrad Bukowicz <konradbukowicz@macbook-pro-konrad.home>
2021-02-01 21:10:40 +0100

    Changes in Java invoice to fill every tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,8 +1,6 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.security.KeyStore.Entry;
-import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -13,7 +11,7 @@ public class Invoice {
     private Map<Product, Integer> products = new LinkedHashMap<>();
     
     // Nie potrzebujemy tu konsturktora, poniewaz mamy to zalatwione na poziomie deklaracji
-    // LinedHashMap
+    // LiknedHashMap
 
     public void addProduct(Product product) {
         products.put(product, 1);
@@ -22,9 +20,14 @@ public class Invoice {
     public void addProduct(Product product, Integer quantity) {
     	products.put(product, quantity);
     	
+    	if(quantity <= 0 || quantity == null) {			// BigDecimal.ZERO to bedzie to samo co new BigDecimal("0")
+    		throw new IllegalArgumentException("You cannot have got no product OR negative number of products (less than 0)");
+    	}
+    	
     }
 
     public BigDecimal getTotalNettoPrice() {
+    	
     	BigDecimal sum = BigDecimal.ZERO;
     	
         for(Product product : this.products.keySet()) {			// Keyset zwraca mi zbior kluczy dla kazdego produktyu zawartego w tej mapie
@@ -35,20 +38,6 @@ public class Invoice {
         
         return sum;
     }
-
-
-	public BigDecimal getTax() {
-		
-		BigDecimal taxSum = BigDecimal.ZERO;
-		
-		for(Product product : this.products.keySet()) {			
-        	Integer quantity = this.products.get(product);	
-        	
-        	taxSum = taxSum.add(product.getTaxPercent().multiply(new BigDecimal(quantity)));
-		}
-		
-		return taxSum;
-	}
 		
     public BigDecimal getTotalBruttoPrice() {
     	
@@ -62,4 +51,9 @@ public class Invoice {
 		
 		return priceWithTaxSum;
     }
+    
+	public BigDecimal getTax() {
+			
+		return getTotalBruttoPrice().subtract(getTotalNettoPrice());
+	}
 }
